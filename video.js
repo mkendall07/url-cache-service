3be04c1160daec66dd58970d284c6b1eee7963e2
Matt Kendall <mkendall@appnexus.com>
2017-01-20 09:33:53 -0500

    initial commit

---
+++ video.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+var cacheManager = require('cache-manager');
+var memoryCache = cacheManager.caching({store: 'memory', max: 100, ttl: 60/*seconds*/});
+var request = require('request');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/put', function (req, res) {
+  if(req.query.id && req.query.url ) {
+    var decodedUrl = decodeURIComponent(req.query.url);
+    console.log(decodedUrl);
+    makeRequest(decodedUrl, req.query.id );
+    res.send('ok');
+  }
+  else{
+    //malformed request
+    res.sendStatus(400);
+  }
+});
+
+app.get('/get', function (req, res) {
+  if(req.query.id) {
+    memoryCache.get(req.query.id, function(err, urlCache) {
+      if(err) {
+        console.log(err);
+        res.sendStatus(500);
+      }
+       if(urlCache) {
+         if(urlCache.contentType) {
+           res.header('Content-Type', urlCache.contentType);
+         }
+         res.send(urlCache.content);
+       }
+       else{
+         res.send('no data found');
+       }
+    });
+  }
+  else{
+    //malformed request
+    res.sendStatus(400);
+  }
+});
+
+app.listen(3001, function () {
+  console.log('Video app listening on port 3001!');
+});
+
+function UrlCache(content, contentType) {
+  this.content = content;
+  this.contentType = contentType;
+}
+
+function makeRequest(url, id) {
+  request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    urlItem = new UrlCache(body, response.headers['content-type']);
+    memoryCache.set(id, urlItem);
+  }
+});
+}
